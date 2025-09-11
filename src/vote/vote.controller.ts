bfcb3354b5cde700c7ea7f43c8979602af9883fd
hsjeong-fanmaum <hs.jeong@fanmaum.com>
2025-09-11 13:38:21 +0900

    refactor: Vote Controller 수정
    Signed-off-by: hsjeong-fanmaum <hs.jeong@fanmaum.com>

--- src/vote/vote.controller.ts
+++ src/vote/vote.controller.ts
@@ -1,8 +1,6 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
 import { VoteService } from './vote.service';
-import { VoteStatus } from './vote.enum';
 import { VoteResultDto } from './vote.response.dto';
-import { ApiQuery } from '@nestjs/swagger';
 import { SearchVoteRequestDto } from './vote.request.dto';
 import { ParseBigIntPipe } from '../common/parse-big-int-pipe.service';
 
@@ -11,10 +9,6 @@ export class VoteController {
   constructor(private voteService: VoteService) {}
 
   @Get('search')
-  @ApiQuery({ name: 'page', required: false })
-  @ApiQuery({ name: 'size', required: false })
-  @ApiQuery({ name: 'status', enum: VoteStatus, required: false })
-  @ApiQuery({ name: 'search', required: false })
   async searchVote(
     @Query()
     { page, size, status, search }: SearchVoteRequestDto,

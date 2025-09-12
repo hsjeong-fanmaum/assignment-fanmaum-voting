766367485486e446b535843813e8df419dc0be9d
hsjeong-fanmaum <hs.jeong@fanmaum.com>
2025-09-12 17:19:57 +0900

    refactor: 회원가입 API 모듈 위치 수정
    Signed-off-by: hsjeong-fanmaum <hs.jeong@fanmaum.com>

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -16,10 +16,24 @@ import { LoginRequestDto } from './auth.request.dto';
 import { LoginResponseDto } from './auth.response.dto';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { User } from './user.decorator';
+import { UserService } from '../user/user.service';
+import { UserResultDto } from '../user/user.response.dto';
+import { SignupRequestDto } from '../user/user.request.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+  ) {}
+
+  @UseInterceptors(ClassSerializerInterceptor)
+  @SerializeOptions({ type: UserResultDto })
+  @Post('/signup')
+  async signup(@Body() signupData: SignupRequestDto): Promise<UserResultDto> {
+    //loginId unique 로직 작성 필요
+    return this.userService.signup(signupData);
+  }
 
   @Post('/login')
   async signIn(

1ef3013e2592dfc57b6b8401eb4b945daa0fafa9
hsjeong-fanmaum <hs.jeong@fanmaum.com>
2025-09-17 10:13:57 +0900

    feat: star 검색 기능 구현
    Signed-off-by: hsjeong-fanmaum <hs.jeong@fanmaum.com>

--- src/star/star.resolver.ts
+++ src/star/star.resolver.ts
@@ -1,12 +1,4 @@
-import {
-  Args,
-  ID,
-  Int,
-  Parent,
-  Query,
-  ResolveField,
-  Resolver,
-} from '@nestjs/graphql';
+import { Args, ID, Int, Parent, Query, ResolveField, Resolver, } from '@nestjs/graphql';
 import { StarDto } from './dto/star.dto';
 import { StarService } from './star.service';
 import { ParseBigIntPipe } from '../common/parse-big-int.pipe';
@@ -20,6 +12,13 @@ export class StarResolver {
     @Args('id', { type: () => ID }, ParseBigIntPipe) id: bigint,
   ): Promise<StarDto> {
     return this.starService.getStarById(id);
+  }
+
+  @Query(() => [StarDto])
+  async searchStar(
+    @Args('q', { type: () => String, nullable: true }) keyword?: string,
+  ): Promise<StarDto[]> {
+    return this.starService.searchStar(keyword);
   }
 
   // 왜 GraphQL은 BigInt를 지원하지 않는가
